750c6c89830aef02a460ea6ed1f85f8c5a433981
macintario <macintario@gmail.com>
2018-08-23 20:52:07 -0500

    Hola mundo

---
+++ segundo.py
@@ -0,0 +1,23 @@
+def sumar(a, b):
+    return(a + b)
+
+
+def restar(a, b):
+    return(a - b)
+
+
+def multiplicar(a, b):
+    return(a * b)
+
+
+def dividir(a, b):
+    return(a / b)
+
+
+a=7
+b=2
+
+print(sumar(a, b))
+print(restar(a, b))
+print(multiplicar(a, b))
+print(dividir(a, b))
