30935427781f0ac42432489df5e99c8cacf1e924
macintario <macintario@gmail.com>
2018-08-23 20:51:12 -0500

    Hola mundo

---
+++ primero.py
@@ -0,0 +1,16 @@
+numeros_enteros=[]
+for i in range(1, 100000):
+    numeros_enteros.append(i)
+print(numeros_enteros)
+
+pares=[]
+impares=[]
+for j in range(1,50000):
+    pares.append(2*j)
+    impares.append(2*j-1)
+miDic=dict(losPares=pares,losImpares=impares)
+print(miDic)
+print(miDic['losPares'])
+print(miDic['losImpares'])
+
+
