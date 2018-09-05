918df0e068c2baf64bc76519a06790d1a6d09b59
macintario <macintario@gmail.com>
2018-09-05 16:29:29 -0500

    Tarea documentada

--- py_mongo.py
+++ py_mongo.py
@@ -1,28 +1,46 @@
-import pymongo
+####################################################################################
+# INSTITUTO POLITECNICO NACIONAL
+# CENTRO DE INVESTIGACION EN COMPUTACION
+# DIPLOMADO EN DESCUBRIMIENTO DEL CONOCIMIENTO CON
+# HERRAMIENTAS BIG DATA
+# PRACTICA: PYTHON CON MYSQL
+# ALUMNO: IVAN GUTIERREZ RODRIGUEZ
+####################################################################################
+
+#importamos la biblioteca para conectarnos a MONGODB
 from pymongo import MongoClient
 
-
+#Nos conectamos a MONGO
 #client = MongoClient('localhost', 27017,username='root',password='Cic1234*',authSource="admin")
 client = MongoClient('localhost', 27017)
+# Usamos la BD test
 db = client["test"]
+# Seleccionamos la colecccion DATA
 collection = db["data"]
 
+# Cargamos los datos
 documentos = collection.find()
 
+# Hacemos un diccionario para guardar las cunentas de los documentos
 cuenta = dict()
 
-nDocs = documentos.count()
+# Iteramos sobre los documentos
 for d in documentos:
-    llaves = d.keys()
-    for k in llaves:
-        if k in cuenta:
-            cuenta[k] += 1
+    campos = d.keys()
+#   Iteramos sobre los campos de los documentos
+    for c in campos:
+        if c in cuenta:
+            cuenta[c] += 1
         else:
-            cuenta[k] = 1
+            cuenta[c] = 1
 
-print("Total de documentos")
-print(cuenta["_id"])  # Todos los documentos tienen _id
-
+# Salida de resultados
+print("-"*60)
+print("Total de documentos:")
+print(documentos.count())
+print("-"*60)
+print("Campos")
 for llave,valor in cuenta.items():
-    print("Llave      :" + llave)
-    print("  Presencia:" + (str) (valor / cuenta["_id"] * 100) + "%")
+    print('%30s' % llave+"     :"+ (str) (valor / documentos.count() * 100) + "%")
+print("-"*60)
+print("Fin del programa")
