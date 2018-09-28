c533074ed24432588bef657b205c808f52ab63b7
macintario <macintario@gmail.com>
2018-09-28 17:00:38 -0500

    Tarea documentada fin

--- logistic reg.py
+++ logistic reg.py
@@ -27,23 +27,35 @@ accuracy = log_model.score(X=pd.DataFrame(dataPeople["Is Female"]),
                            y=dataPeople["Buy"])
 print("accuracy")
 print (accuracy)
-train_features = pd.DataFrame([dataPeople["Is Female"],
-                              dataPeople["Is Married"],
-                              dataPeople["Is Professional"]]).T
+
+print('########## MULTI ################')
+#normalize income
+dataPeople["Income"]=dataPeople["Income"]/dataPeople["Income"].max()
+dataPeople["Residence Length"]=dataPeople["Residence Length"]/dataPeople["Residence Length"].max()
+
+train_features = pd.DataFrame([
+                              dataPeople["Income"],          #1
+                              dataPeople["Is Female"],       #2
+                              #dataPeople["Is Married"],      #3
+                              #dataPeople["Has College"],     #4
+                              #dataPeople["Is Professional"], #5
+                              #dataPeople["Is Retired"],      #6
+                              #dataPeople["Unemployed"],      #7
+                              #dataPeople["Residence Length"], #8
+                              #dataPeople["Dual Income"],     #9
+                              #dataPeople["Minors"],          #10
+                              #dataPeople["Own"],              #11
+                              #dataPeople["House"],           #12
+                              #dataPeople["White"],            #13
+                              #dataPeople["English"],          #14
+                              #dataPeople["Prev Child Mag"],    #15
+                              #dataPeople["Prev Parent Mag"],  #16
+                               ]).T
 
 print("Caracteristicas de entrenamiento")
 print (train_features)
 print ("inicializar el modelo")
 log_model = linear_model.LogisticRegression()
-print("Entrenar el modelo")
-log_model.fit(X = train_features ,
-             y = dataPeople["Buy"])
-print("Intercección ")
-print(log_model.intercept_)
-print("Coeficientes")
-print(log_model.coef_)
-print("Realizar predicciones")
-preds = log_model.predict(X= train_features)
 print("Entrenar el modelo")
 log_model.fit(X = train_features ,
              y = dataPeople["Buy"])
@@ -53,15 +65,8 @@ print("Coeficientes")
 print(log_model.coef_)
 print("Realizar predicciones")
 preds = log_model.predict(X= train_features)
-print("Entrenar el modelo")
-log_model.fit(X = train_features ,
-             y = dataPeople["Buy"])
-print("Intercección ")
-print(log_model.intercept_)
-print("Coeficientes")
-print(log_model.coef_)
-print("Realizar predicciones")
-preds = log_model.predict(X= train_features)
+print("#######################################################33")
+print(train_features[:0])
 print("Matriz de confusion")
 tablePred=pd.crosstab(preds,dataPeople["Buy"])
 print (tablePred)
