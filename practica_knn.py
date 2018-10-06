7d742722775b95a98b8702c931b2910af41fd985
macintario <macintario@gmail.com>
2018-10-06 12:20:04 -0500

    nomalizacion

--- practica_knn.py
+++ practica_knn.py
@@ -5,16 +5,21 @@ def normaliza_min_max (columna):
     maximo = columna.max()
     minimo = columna.min()
     columna = (columna-minimo+0.)/(maximo-minimo+0.) # Se agrega "0." para evitar división entera
-    print(columna)
+    #print(columna)
     return columna
 
 #Leemos datos de entrenamiento
 entrenamiento = pd.read_csv("entrenamiento.csv")
+normalizado = entrenamiento
 #Normalizamos
-entrenamiento["Precio"]=normaliza_min_max(entrenamiento["Precio"])
-entrenamiento["Metros Cuadrados"]=normaliza_min_max(entrenamiento["Metros Cuadrados"])
-entrenamiento["Baños"]=normaliza_min_max(entrenamiento["Baños"])
-entrenamiento["Cuartos"]=normaliza_min_max(entrenamiento["Cuartos"])
-entrenamiento["Estacionamiento"]=normaliza_min_max(entrenamiento["Estacionamiento"])
-entrenamiento["Mantenimiento"]=normaliza_min_max(entrenamiento["Mantenimiento"])
+normalizado["Precio"]=normaliza_min_max(entrenamiento["Precio"])
+normalizado["Metros Cuadrados"]=normaliza_min_max(entrenamiento["Metros Cuadrados"])
+normalizado["Baños"]=normaliza_min_max(entrenamiento["Baños"])
+normalizado["Cuartos"]=normaliza_min_max(entrenamiento["Cuartos"])
+normalizado["Estacionamiento"]=normaliza_min_max(entrenamiento["Estacionamiento"])
+normalizado["Mantenimiento"]=normaliza_min_max(entrenamiento["Mantenimiento"])
 
+
+
+
+print("OK")
